5dbd46d4ca4e6fbba72399f6df37ab415ad9b5c5
FougereRegent <damien.venant@outlook.com>
2026-08-30 15:24:10 +0200

    chore: add pnpm

--- src/backend/api/Ragkivio.Aspire/AppHost.cs
+++ src/backend/api/Ragkivio.Aspire/AppHost.cs
@@ -17,6 +17,7 @@ var password = builder.AddParameter("password", secret: true);
 var rabbitmq = builder.AddRabbitMQ("broker", username, password)
                 .WithManagementPlugin();
 
+
 var graphqlApi = builder.AddProject<Projects.Ragkivio_Graphql>("graphql-api")
                     .WithOtlpExporter()
                     .WaitFor(db)
@@ -37,5 +38,14 @@ var graphqlApi = builder.AddProject<Projects.Ragkivio_Graphql>("graphql-api")
                     })
                     .WithUrl("/scalar")
                     .WithUrl("/graphql");
+
+var frontend = builder.AddViteApp("frontend", "../../../frontend/ragivio-web")
+                    .WithPnpm()
+                    .WithRunScript("dev:custom")
+                    .WithReference(graphqlApi)
+                    .WithEndpoint("http", endpoint => endpoint.Port = 5173)
+                    .WaitFor(graphqlApi)
+                    .WithEnvironment("VITE_DEV", "true")
+                    .WithEnvironment("VITE_APP_VERSION", "1.0.0");
 
 builder.Build().Run();
